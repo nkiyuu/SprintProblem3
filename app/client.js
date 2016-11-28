5b79b48d87b71c6eb475ac46451e9b291b7cf900
Yuki Nozaki <yuki_nozaki@shino.ecei.tohoku.ac.jp>
2016-11-28 21:20:11 +0900

    サーバー用に設定を変更

--- app/client.js
+++ app/client.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var ws = new WebSocket('ws://localhost:3000');
+var ws = new WebSocket('ws://http://ec2-35-160-74-111.us-west-2.compute.amazonaws.com/:3000');
 
 $(function () {
-  $('form').submit(function(){
-    var $this = $(this);
-    // ws.onopen = function() {
-    //   console.log('sent message: %s', $('#m').val());
-    // };
-    ws.send($('#m').val());
-    $('#m').val('');
-    return false;
-  });
-  ws.onmessage = function(msg){
-    var resp = JSON.parse(msg.data);
-    $('#messages')
-      .append($('<li>')
-      .append($('<span class="message">').text(resp.text)));
-  };
-  ws.onerror = function(err){
-    console.log("err", err);
-  };
-  ws.onclose = function close() {
-    console.log('disconnected');
-  };
+    $('form').submit(function () {
+        var $this = $(this);
+        ws.onopen = function() {
+           console.log('sent message: %s', $('#m').val());
+        };
+        ws.send($('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    ws.onmessage = function (msg) {
+        var resp = JSON.parse(msg.data);
+        $('#messages')
+            .append($('<li>')
+                .append($('<span class="message">').text(resp.text)));
+    };
+    ws.onerror = function (err) {
+        console.log("err", err);
+    };
+    ws.onclose = function close() {
+        console.log('disconnected');
+    };
 });
